906d7d55e6f59f0086a2449c4df0c402468a924e
lap <lap@DESKTOP-E9ARFLP>
2023-09-30 10:20:28 +0530

    Added NON BDD POST,PUT,DELETE API requests

--- api.testing.project/src/test/java/com/restassured/nonbdd/GetAllEmployees.java
+++ api.testing.project/src/test/java/com/restassured/nonbdd/GetAllEmployees.java
@@ -1,5 +1,7 @@
 package com.restassured.nonbdd;
 
+import java.security.cert.CertStoreSpi;
+
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -9,17 +11,71 @@ import io.restassured.specification.RequestSpecification;
 
 public class GetAllEmployees {
 
-	@Test
+	@Test(enabled = true,priority = 1)
 	public void getAllEmployees() {
 
 		RestAssured.baseURI = "http://localhost:3000/";
 
 		RequestSpecification requestSpecification = RestAssured.given();
 
-		Response response = requestSpecification.request(Method.GET,"employees");
-		
+		Response response = requestSpecification.request(Method.GET,"employees/");
+
 		System.out.println(response.asPrettyString());
 		System.out.println(response.getStatusLine());
-	
+
+	}
+
+	@Test(enabled = true,priority = 2)
+	public void createAnEmployee() {
+		RestAssured.baseURI = "http://localhost:3000/";
+		RequestSpecification requestSpecification =	RestAssured.given().header("Content-Type","application/json")
+				.body("{\r\n"
+						+ "      \"first_name\": \"John\",\r\n"
+						+ "      \"last_name\": \"Mount\",\r\n"
+						+ "      \"email\": \"john@gmail.com\"\r\n"
+						+ "    }");
+		Response response =	requestSpecification.request(Method.POST,"employees/");
+		System.out.println(response.asPrettyString());
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getStatusLine());
+	}
+
+
+	@Test(priority = 3)
+	public void updateAnEmployee() {
+		RestAssured.baseURI = "http://localhost:3000/";
+		RequestSpecification requestSpecification = RestAssured.given().header("Content-Type","application/json")
+				.body("{\r\n"
+						+ "    \"first_name\": \"Agni\",\r\n"
+						+ "    \"last_name\": \"Prasath\",\r\n"
+						+ "    \"email\": \"agniprasath@gmail.com\"\r\n"
+						+ "  }");
+
+		Response response = requestSpecification.request(Method.PUT,"employees/2/");
+		System.out.println(response.asPrettyString());
+		System.out.println(response.contentType());
+		System.out.println(response.getStatusLine());
+
+	}
+
+	@Test(priority = 4)
+	public void deleteAnEmployee() {
+		RestAssured.baseURI = "http://localhost:3000/";
+		RequestSpecification requestSpecification = RestAssured.given();
+
+		//employee 5 is deleted
+		Response response = requestSpecification.request(Method.DELETE,"employees/5");
+
+		System.out.println(response.asPrettyString());
+		System.out.println(response.getStatusLine());
+	}
+
+	@Test(priority = 5)
+	public void getAnEmployee() {
+		RestAssured.baseURI = "http://localhost:3000/";
+		RequestSpecification requestSpecification = RestAssured.given();
+		Response response = requestSpecification.request(Method.GET,"employees/1");
+		System.out.println(response.asPrettyString());
+		System.out.println(response.getStatusLine());
 	}
 }
